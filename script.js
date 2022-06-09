cae51bac60f9f4f9bee5e014104d9240f4ddf0dc
Hugo-Rvs <hugo.rivas31000@gmail.com>
2022-06-09 18:20:01 +0200

    finalisation questionnaire

---
+++ script.js
@@ -0,0 +1,124 @@
+let button1 = document.querySelector("#button1");
+let bravo1 = document.querySelector(".bravo1")
+
+bravo1.style.display = "none";
+
+button1.onclick = () => {
+    bravo1.style.display = "block";
+};
+
+
+
+
+let button2 = document.querySelector("#button2");
+let bravo2 = document.querySelector(".bravo2")
+
+bravo2.style.display = "none";
+
+button2.onclick = () => {
+    bravo2.style.display = "block";
+};
+
+
+
+
+let button3 = document.querySelector("#button3");
+let bravo3 = document.querySelector(".bravo3")
+
+bravo3.style.display = "none";
+
+button3.onclick = () => {
+    bravo3.style.display = "block";
+};
+
+
+
+
+let button4 = document.querySelector("#button4");
+let bravo4 = document.querySelector(".bravo4")
+
+bravo4.style.display = "none";
+
+button4.onclick = () => {
+    bravo4.style.display = "block";
+};
+
+
+
+
+let button5 = document.querySelector("#button5");
+let bravo5 = document.querySelector(".bravo5")
+
+bravo5.style.display = "none";
+
+button5.onclick = () => {
+    bravo5.style.display = "block";
+};
+
+
+
+
+let button6 = document.querySelector("#button6");
+let bravo6 = document.querySelector(".bravo6")
+
+bravo6.style.display = "none";
+
+button6.onclick = () => {
+    bravo6.style.display = "block";
+};
+
+
+
+
+let button7 = document.querySelector("#button7");
+let bravo7 = document.querySelector(".bravo7")
+
+bravo7.style.display = "none";
+
+button7.onclick = () => {
+    bravo7.style.display = "block";
+};
+
+
+
+let button8 = document.querySelector("#button8");
+let bravo8 = document.querySelector(".bravo8")
+
+bravo8.style.display = "none";
+
+button8.onclick = () => {
+    bravo8.style.display = "block";
+};
+
+
+
+let button9 = document.querySelector("#button9");
+let bravo9 = document.querySelector(".bravo9")
+
+bravo9.style.display = "none";
+
+button9.onclick = () => {
+    bravo9.style.display = "block";
+};
+
+
+
+let button10 = document.querySelector("#button10");
+let bravo10 = document.querySelector(".bravo10")
+
+bravo10.style.display = "none";
+
+button10.onclick = () => {
+    bravo10.style.display = "block";
+};
+
+
+
+let button11 = document.querySelector("#button11");
+let bravo11 = document.querySelector(".bravo11")
+
+bravo11.style.display = "none";
+
+button11.onclick = () => {
+    bravo11.style.display = "block";
+};
